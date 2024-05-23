fb3a29e1148ba75fff50af3a5501622ba0fa2da8
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-05-23 23:38:22 +0500

    OOP Done

--- 00_Object-Oriented-Programming/08_Polymorphism.js
+++ 00_Object-Oriented-Programming/08_Polymorphism.js
@@ -4,6 +4,17 @@
 *           2- Compile Time & Run Time Polymorphism.
 *
 *           Compile-Time: 
+                ==> Before Excecution of Code, Our code is syntactically and semantically Correct.
+            Run-Time :
+                ==> After Exceution, how our Code behaves is its Run time Excecution.
+
+    Types of Polymorphism:
+        1- Compile-Time Poly...:
+                ==> Before Exceution of Code, we have mulitple methods with same name but different parameters, shows polymorphism.
+        2- Run-Time Poly...:
+                ==> After exceution, we find that which method is exceuted like in Method overriding.
+
+            
 * */
 
 
@@ -22,6 +33,22 @@ class Company{
     get displayCurrentContext(){
         return this
     }
+
+    static get displayClassCurrContext(){
+        return this
+    }
+
+    
+    // Method Overloading:
+    companyDetails(){
+        return "This Company have this this this"
+    }
+    companyDetails(location){
+        return `${location} and this this this`
+    }
+
+    
+
 }
 
 class ProductsInventory extends Company{
@@ -30,6 +57,15 @@ class ProductsInventory extends Company{
 
     static get classDataMembers(){
         return [this.ProductInventoryTypes, this.companyName]
+    }
+
+    constructor(mode, location, categories, Products_Details = new Array()){
+        super(mode, location, categories)
+        this.prodDetails = Products_Details
+    }
+
+    companyDetails(location){
+        return "Method Overrided which is Overloaded in Parent Class " + location
     }
 
 }
@@ -42,6 +78,12 @@ class ProductsInventory extends Company{
     obj.categories = "Women Wear"
 
     console.log(obj.displayCurrentContext)
+    console.log(Company.displayClassCurrContext)
 
-    console.log(Company)
+    console.log(ProductsInventory.classDataMembers);
+
+    const prodObj = new ProductsInventory()
+    console.log(prodObj.companyDetails())
+
+
 })()
---
+++ 00_Object-Oriented-Programming/Abstraction.md
@@ -0,0 +1,11 @@
+# Abstaction:
+<p>Hiding the Complex Implementation of System and just providing essiential features</p>
+
+<h3>Example:</h3>
+<p>==> In a Car, you are only expose to Driving / High-level features and all other components are hidden</p>
+<p>==> In Email System, you are provided with high level Functionalites not exposing SMTP Protocol or network process.</p>
+
+<h3>Features: </h3>
+<p>==> Fouces on What, not How.</p>
+<p>==> Simplicity, High Level Overview</p>
+<p>==> Obtain through Abstract Classes</p>
